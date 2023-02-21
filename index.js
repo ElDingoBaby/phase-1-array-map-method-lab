51fa2f88f3b54525beb7a38653780d11729aa4de
Alexis Sanchez <alexiss574@gmail.com>
2023-02-21 14:32:52 -0800

    completed lab

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+function titleCased() {
+  return tutorials.map(tutorial => tutorial.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' '))
 
-const titleCased = () => {
-  return tutorials
 }
+
+console.log(titleCased())
